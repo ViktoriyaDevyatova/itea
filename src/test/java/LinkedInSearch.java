75fc428488234b4dd6f820b0fca8316fd88c343b
ViktoriyaDevyatova <v.devyatova@gmail.com>
2018-02-23 21:14:08 +0200

    LinkedInSearch with scroll . But contains 1 defect - take only 9 elements for HR verification

--- src/test/java/LinkedInSearch.java
+++ src/test/java/LinkedInSearch.java
@@ -44,32 +44,30 @@ public class LinkedInSearch {
         String searchWord = "hr";
         searchField.sendKeys(searchWord);
         clickButton.click();
+
         sleep(5000);
 
-      //  WebElement listOfResults = webDriver.findElement(By.xpath("//ul[contains(@class, 'search-results__list list-style-none')]"));
-       //listOfResults.findElement(By.tagName("li")).findElement(By.className("search-result search-result__occluded-item ember-view"));
+        List<WebElement> listOfResults = webDriver.findElements(By.xpath("//li[contains(@class, 'search-result__occluded-item')]"));
+      //  int jj = listOfResults.size();
+//        JavascriptExecutor js = ((JavascriptExecutor) webDriver);
+//        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+//        js.executeScript("window.scrollBy(0,250)", "");
 
-        //search-result__info pt3 pb4 ph0
-        JavascriptExecutor js = ((JavascriptExecutor) webDriver);
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
-        js.executeScript("window.scrollBy(0,250)", "");
 
-        List<WebElement> listOfResults = webDriver.findElements(By.cssSelector("div[class=\"search-result__info pt3 pb4 ph0\"]"));
+       // List<WebElement> listOfResults = webDriver.findElements(By.cssSelector("div[class=\"search-result__info pt3 pb4 ph0\"]"));
         System.out.println("how much results: " + listOfResults.size());
+        Assert.assertEquals(listOfResults.size(), 10, "Results of the search do not contain HR" );
 
-      Assert.assertEquals( listOfResults.size(), 4, "Results of the search do not contain HR" );
+        for (int i =1; i < listOfResults.size(); i++){
+            ((JavascriptExecutor)webDriver).executeScript("arguments[0].scrollIntoView();", listOfResults.get(i));
+            String cardTitle = webDriver.findElement(By.xpath("//li[contains(@class, 'search-result__occluded-item')]["+i+"]//span[contains(@class, 'actor-name')]")).getText();
+            Assert.assertTrue(cardTitle.toLowerCase().contains(searchWord),"Search result does not contain HR in:" + Integer.toString(i));
+            System.out.println(cardTitle);
 
-        for (WebElement result : listOfResults)   {
-        Assert.assertTrue(result.getText().toLowerCase().contains(searchWord),"Search result does not contain HR");
-                              // System.out.println("element: " + result.getText());
-  }
-
-
-
+        }
 
 
-        //HOMEWORK
-        //check quantity of results -10 and all results contain HR
+
 
 
 
